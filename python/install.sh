633929f2b076d7cec2c436b7a88b10f89642bf2e
Jim Zhang <jim.zoumo@gmail.com>
2016-05-16 15:12:15 +0800

    [ -> [[
    将test语句中的单中括号换成双中括号

--- python/install.sh
+++ python/install.sh
@@ -15,11 +15,11 @@ plugins=(
 sudo pip install --upgrade pip
 
 # pyenv
-if [ ! -d ${HOME}/.pyenv ]; then
+if [[ ! -d ${HOME}/.pyenv ]]; then
     curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 fi
 
-if [ $OS == "centos" ]; then
+if [[ $OS == "centos" ]]; then
     sudo yum install python-devel.x86_64
 fi
 
