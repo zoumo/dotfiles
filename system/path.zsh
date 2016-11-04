2d47d37482dd2a5ab9495125dd4a6af446d3a112
Jim Zhang <jim.zoumo@gmail.com>
2016-11-04 15:43:50 +0800

    detect os

--- system/path.zsh
+++ system/path.zsh
@@ -1,8 +1,3 @@
-if [[ "$(uname -s)" == "Darwin" ]]; then
-    export OS="osx"
-elif [[ "$(uname -s)" == "Linux" ]] && [[ -n "$(which yum)" ]]; then
-    export OS="centos"
-fi
 export GOPATH="$HOME/.golang"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:${GOPATH}/bin/:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
