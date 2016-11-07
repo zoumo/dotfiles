b658e195f6d6b87ba0a2fe50707b7689a5593636
Jim Zhang <jim.zoumo@gmail.com>
2016-11-07 10:30:50 +0800

    load os.sh in path.zsh

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,4 @@
+source $HOME/.dotfiles/system/os.sh
 export GOPATH="$HOME/.golang"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:${GOPATH}/bin/:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
