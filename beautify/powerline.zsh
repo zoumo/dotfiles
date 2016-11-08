23c082a4eb2abcde2d5b51dc1da6480eb45339c7
Jim Zhang <jim.zoumo@gmail.com>
2016-11-08 14:39:38 +0800

    docs: add compatibility

--- beautify/powerline.zsh
+++ beautify/powerline.zsh
@@ -2,5 +2,5 @@
 if command_exists pip; then
 #    export POWERLINE_ROOT="$(pip show powerline-status | grep Location | cut -d " " -f 2)/powerline"
     export POWERLINE_ROOT="$(python -c "from distutils.sysconfig import get_python_lib; print get_python_lib()")/powerline"
-    . ${POWERLINE_ROOT}/bindings/zsh/powerline.zsh
+    source ${POWERLINE_ROOT}/bindings/zsh/powerline.zsh
 fi
