729066c3b9de45a2ce77cca2fc674305c3196cfa
Jim Zhang <jim.zoumo@gmail.com>
2016-11-08 11:35:24 +0800

    fix(path): do not print export msg

--- system/path.zsh
+++ system/path.zsh
@@ -1,6 +1,6 @@
 # export DOTFILES=$HOME/.dotfiles
 source $HOME/.dotfiles/system/os.sh
-export -f command_exists
+export -f command_exists >/dev/null 2>&1
 
 export GOPATH="$HOME/.golang"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:${GOPATH}/bin/:${HOME}/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
