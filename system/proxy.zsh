54ed70412dc230341eb73fae81f7020ca7e5a2dc
Jim Zhang <jim.zoumo@gmail.com>
2017-03-31 17:17:19 +0800

    feat: add terminal proxy support

---
+++ system/proxy.zsh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+function proxy_on() {
+    export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
+    export http_proxy="http://127.0.0.1:1087"
+    export https_proxy=$http_proxy
+    echo "=> terminal proxy on"
+    echo "forward to 127.0.0.1:1087."
+    echo "please check your privoxy listening on 1087"
+}
+
+function proxy_off(){
+    unset http_proxy
+    unset https_proxy
+    echo "=> terminal proxy off"
+}
