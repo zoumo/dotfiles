a62c8cee4caf816d09f6e86276064da0ee48c48d
Jim Zhang <jim.zoumo@gmail.com>
2017-08-17 18:08:04 +0800

    fix(ruby): remove ruby source

--- ruby/install.sh
+++ ruby/install.sh
@@ -4,7 +4,3 @@ set -e
 
 ROOT_PATH="$(dirname $(dirname ${BASH_SOURCE}))"
 source $ROOT_PATH/lib/lib.sh
-
-if command_exists gem; then
-    gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org/
-fi
