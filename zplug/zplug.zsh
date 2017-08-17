14e37132516ac8a44835a80f16cfc8486936a559
Jim Zhang <jim.zoumo@gmail.com>
2017-08-17 18:07:12 +0800

    feat(zplug): remove rupa/z && add zsh-users/zsh-autosuggestions

--- zplug/zplug.zsh
+++ zplug/zplug.zsh
@@ -5,10 +5,10 @@ fi
 
 if command_exists zplug; then
     zplug "changyuheng/fz", defer:1
-    zplug "rupa/z", use:z.sh
+    # zplug "rupa/z", use:z.sh
     # zplug "changyuheng/zsh-interactive-cd", use:zsh-interactive-cd.plugin.zsh, from:github
-    zplug "pindexis/marker", from:github, hook-build:"python install.py"
-
+    zplug "pindexis/marker", hook-build:"python install.py"
+    zplug "zsh-users/zsh-autosuggestions", use:zsh-autosuggestions.zsh
 
 
 	if ! zplug check; then
@@ -20,9 +20,15 @@ if command_exists zplug; then
     # source marker
     [[ -s "$HOME/.local/share/marker/marker.sh" ]] && source "$HOME/.local/share/marker/marker.sh"
 
+    # zsh-autosuggestions
+    # set highlight foreground colour to 
+    export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=243"
+
 fi
 
 if command_exists fasd; then
+    eval "$(fasd --init auto)"
+
     alias a='fasd -a'        # any
     alias s='fasd -si'       # show / search / select
     alias d='fasd -d'        # directory
