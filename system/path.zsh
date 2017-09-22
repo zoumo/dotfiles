7e01cbc4a6d00924c34b4d17aba666493886ed0f
Jim Zhang <jim.zoumo@gmail.com>
2017-09-22 21:05:47 +0800

    perf(path): combine path files

--- system/path.zsh
+++ system/path.zsh
@@ -59,3 +59,25 @@ if [[ $OS == "macos" ]]; then
     export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
     export JAVA_HOME=$(/usr/libexec/java_home)
 fi
+
+# ====================================================================
+# python
+# ====================================================================
+# virtualenv config
+export VIRTUALENV_USE_DISTRIBUTE=1
+export VIRTUALENV_NO_SITE_PACKAGES=1  # 设置所有虚拟环境与系统site-packages进行隔离
+
+# pyenv
+export PYENV_ROOT="${HOME}/.pyenv"
+export PATH="${PYENV_ROOT}/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+export PYTHONDONTWRITEBYTECODE=x
+
+# thefuck
+eval $(thefuck --alias)
+
+# ====================================================================
+# zplug
+# ====================================================================
+export ZPLUG_HOME=/usr/local/opt/zplug
