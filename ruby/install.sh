588cc6d7aeae3ad2d3fbf982a73a4e96aa2835a3
Jim Zhang <jim.zoumo@gmail.com>
2017-10-13 01:21:21 +0800

    refactor: install language support firstly

--- ruby/install.sh
+++ ruby/install.sh
@@ -4,3 +4,9 @@ set -e
 
 ROOT_PATH="$(dirname $(dirname ${BASH_SOURCE}))"
 source $ROOT_PATH/lib/lib.sh
+
+if [[ $OS == "macos" ]]; then
+	brew_install ruby
+elif [[ $OS == "centos" ]]; then
+	sudo yum install -y ruby
+fi
