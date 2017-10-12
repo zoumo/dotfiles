3cf0ea67697af4fe52f4d719ea11a343f6d54c23
Jim Zhang <jim.zoumo@gmail.com>
2017-10-13 00:43:53 +0800

    fix(go): syntax error

--- go/install.sh
+++ go/install.sh
@@ -8,7 +8,7 @@ source $ROOT_PATH/lib/lib.sh
 if [[ $OS == "macos" ]]; then
 	brew_install go mercurial
 	# bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-fi [[ $OS == "centos" ]]; then
+elif [[ $OS == "centos" ]]; then
 	sudo yum install -y golang
 fi
 
