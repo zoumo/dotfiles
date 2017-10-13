1d1a3387108817bbf62b4e55726e6397dca00408
Jim Zhang <jim.zoumo@gmail.com>
2017-10-13 16:02:55 +0800

    fix(ruby): fix rvm in centos

--- ruby/install.sh
+++ ruby/install.sh
@@ -10,6 +10,11 @@ VERSION="2.4"
 if ! command_exists rvm; then
 	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 	curl -sSL https://get.rvm.io | bash -s stable
-	source $HOME/.rvm/scripts/rvm
+
+	if [[ $OS == "macos" ]]; then
+		source $HOME/.rvm/scripts/rvm
+	elif [[ $OS == "centos" ]]; then	
+		source /etc/profile.d/rvm.sh
+	fi
 	rvm install $VERSION 
 fi
