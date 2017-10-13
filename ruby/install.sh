96c74eb0fce918c88e6620efc2b165659c9aa071
Jim Zhang <jim.zoumo@gmail.com>
2017-10-13 15:51:31 +0800

    feat: use rvm manage ruby

--- ruby/install.sh
+++ ruby/install.sh
@@ -5,8 +5,11 @@ set -e
 ROOT_PATH="$(dirname $(dirname ${BASH_SOURCE}))"
 source $ROOT_PATH/lib/lib.sh
 
-if [[ $OS == "macos" ]]; then
-	brew_install ruby
-elif [[ $OS == "centos" ]]; then
-	sudo yum install -y ruby
+VERSION="2.4"
+
+if ! command_exists rvm; then
+	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+	curl -sSL https://get.rvm.io | bash -s stable
+	source $HOME/.rvm/scripts/rvm
+	rvm install $VERSION 
 fi
