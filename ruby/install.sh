0c203f867d9d8404a6afe69e62c2f3b909a09276
Jim Zhang <jim.zoumo@gmail.com>
2017-10-13 16:15:32 +0800

    feat(ruby): use 2.4.1 as default version

--- ruby/install.sh
+++ ruby/install.sh
@@ -5,7 +5,7 @@ set -e
 ROOT_PATH="$(dirname $(dirname ${BASH_SOURCE}))"
 source $ROOT_PATH/lib/lib.sh
 
-VERSION="2.4"
+VERSION="2.4.1"
 
 if ! command_exists rvm; then
 	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
@@ -18,3 +18,5 @@ if ! command_exists rvm; then
 	fi
 	rvm install $VERSION 
 fi
+
+rvm use $VERSION --default
