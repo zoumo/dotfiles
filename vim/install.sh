dddd9942bd6e1449287de610662e320dc06d9173
Jim Zhang <jim.zoumo@gmail.com>
2017-10-16 11:39:39 +0800

    fix(vim): install YouCompleteMe manually

--- vim/install.sh
+++ vim/install.sh
@@ -33,8 +33,20 @@ fi
 # brew install --HEAD neovim
 # pip install neovim
 
+if [[ ! -e ${plugins}/YouCompleteMe.tar.gz ]]; then
+    wget -O ${plugins}/YouCompleteMe.tar.gz "http://ohpunyak1.bkt.clouddn.com/YouCompleteMe.tar.gz?v=9999"
+fi
+
 if [[ ! -d ${plugins}/k-vim ]]; then
     git clone https://github.com/zoumo/k-vim.git
     cd k-vim/
+    if [[ ! -d bundle/YouCompleteMe ]]; then
+        mkdir -p bundle
+        cd bundle 
+        tar -zxf ${plugins}/YouCompleteMe.tar.gz
+        cd YouCompleteMe
+        ./install.py --clang-completer
+        cd .${plugins}/k-vim
+    fi
     sh install.sh
 fi
