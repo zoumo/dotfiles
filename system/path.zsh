ec431ae5e772a39d03d8f26bd730adfe0bc3600f
Jim Zhang <jim.zhang@caicloud.io>
2018-08-23 17:50:21 +0800

    fix: missing pyenv

--- system/path.zsh
+++ system/path.zsh
@@ -87,9 +87,10 @@ export VIRTUALENV_USE_DISTRIBUTE=1
 export VIRTUALENV_NO_SITE_PACKAGES=1 # 设置所有虚拟环境与系统site-packages进行隔离
 
 # pyenv
+export PYENV_ROOT="${HOME}/.pyenv"
+export PATH="${PYENV_ROOT}/bin:${PATH}"
+
 if command_exists pyenv; then
-	export PYENV_ROOT="${HOME}/.pyenv"
-	export PATH="${PYENV_ROOT}/bin:${PATH}"
 	eval "$(pyenv init -)"
 	eval "$(pyenv virtualenv-init -)"
 fi
