33ba52f30f82ed169e0697f267f6ab2d22c78e24
Jim Zhang <jim.zhang@caicloud.io>
2018-12-17 17:14:17 +0800

    feat(tmux): update tmux config

--- system/alias.zsh
+++ system/alias.zsh
@@ -8,9 +8,27 @@ alias la="ls -lAh"
 if [[ "$(os_lsb_dist)" == "macos" ]]; then
 	alias rm="trash"
 fi
-alias mux="tmuxinator"
 
-alias npm="npm --registry=https://registry.npm.taobao.org \
---cache=$HOME/.npm/.cache/cnpm \
---disturl=https://npm.taobao.org/dist \
---userconfig=$HOME/.cnpmrc"
+# 首先在 tmux.conf 配置了
+#     set -g default-terminal "screen-256color"
+#     set -ag terminal-overrides ',screen-256color:Tc'
+#
+# TERM=xterm-256color tmux -2 启动的 session 中 
+# echo $TERM -> screen-256color 说明配置生效了
+# 但是 vim 的配色不正确, 这很奇怪
+#
+# TERM=screen-256color tmux -2 启动的 session 中 
+# echo $TERM -> screen-256color
+# vim 的配色正确
+# 
+# 只有启动 tmux 的时候 TERM 与 tmux 中相同，tmux 中的 vim 才能显示正确的配色
+# 
+# tmux -2 force tmux to assume that the terminal support 256 colors
+alias tmuxinator="TERM=screen-256color tmuxinator"
+alias mux="tmuxinator"
+alias tmux="TERM=screen-256color tmux -2"
+
+# alias npm="npm --registry=https://registry.npm.taobao.org \
+# --cache=$HOME/.npm/.cache/cnpm \
+# --disturl=https://npm.taobao.org/dist \
+# --userconfig=$HOME/.cnpmrc"
