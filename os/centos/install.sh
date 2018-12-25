8cf2007db609801f9718a7b45e62df7e299c2511
Jim Zhang <jim.zhang@caicloud.io>
2018-12-25 11:58:03 +0800

    feat: compile vim from source

--- os/centos/install.sh
+++ os/centos/install.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 binaries=(
-    yum-axelget
-    git
-    dconf
-    gcc
-    clang
-    bzip2
-    openssl-devel
-    libyaml-devel
-    libffi-devel
-    readline-devel
-    zlib-devel
-    gdbm-devel
-    libevent-devel
-    ncurses-devel
-    deltarpm
-    python-devel
-    the_silver_searcher
-    cmake
-    zsh
+	yum-axelget
+	git
+	dconf
+	gcc
+	clang
+	bzip2
+	openssl-devel
+	libyaml-devel
+	libffi-devel
+	readline-devel
+	zlib-devel
+	gdbm-devel
+	libevent-devel
+	ncurses-devel
+	deltarpm
+	python-devel
+	the_silver_searcher
+	cmake
+	zsh
 )
 
 # EPEL(http://fedoraproject.org/wiki/EPEL) 是由 Fedora 社区打造，
@@ -28,6 +28,15 @@ binaries=(
 sudo yum -y update
 sudo yum remove -y epel-release
 sudo yum install -y epel-release
+
+# https://centos.pkgs.org/7/forensics-x86_64/cert-forensics-tools-release-7.noarch.rpm.html
+# rpm -ivh https://forensics.cert.org/cert-forensics-tools-release-el7.rpm
+# sudo yum --enablerepo=forensics install cert-forensics-tools-release
+
+# # remi 源
+# rpm -ivh https://mirrors.tuna.tsinghua.edu.cn/remi/enterprise/remi-release-7.rpm
+# # 修改第一个 enabled=0 为 enabled=1
+# sed -i ':a;N;$!ba;s/enabled=0/enabled=1/' /etc/yum.repos.d/remi.repo
 
 sudo yum groupinstall -y 'Development Tools'
 
--- vim/install.sh
+++ vim/install.sh
@@ -20,12 +20,34 @@ elif [[ $(OS::LSBDist) == 'centos' ]]; then
 	if Command::Exists vim; then
 		vim_version="$(vim --version | head -1 | cut -f 5 -d " ")"
 		if [[ "${MINIMUM_VIM_VERSION}" != $(echo -e "${MINIMUM_VIM_VERSION}\n${vim_version}" | sort -s -t. -k 1,1n -k 2,2n | head -n1) ]]; then
+
+			# ruby  - ryenv
+			# python3 - pyenv
+			sudo yum install -y lua lua-devel luajit \
+				luajit-devel ctags git tcl-devel \
+				perl perl-devel perl-ExtUtils-ParseXS \
+				perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
+				perl-ExtUtils-Embed
+			# This step is needed to rectify an issue with how Fedora 20 installs XSubPP:
+			# symlink xsubpp (perl) from /usr/bin to the perl dir
+			sudo ln -s /usr/bin/xsubpp /usr/share/perl5/ExtUtils/xsubpp
+
 			git::clone https://github.com/vim/vim.git
 			cd vim
+
 			make distclean # if you build Vim before
-			make -j8
+			./configure --with-features=huge \
+				--enable-multibyte \
+				--enable-rubyinterp=yes \
+				--enable-python3interp=yes \
+				--with-python3-config-dir=$(python3-config --configdir) \
+				--enable-perlinterp=yes \
+				--enable-luainterp=yes \
+				--enable-gui=gtk2 \
+				--enable-cscope \
+				--prefix=/usr/local
 			sudo make install
-			cp src/vim /usr/bi
+			cp src/vim /usr/local/bin/vim
 		fi
 	fi
 fi
