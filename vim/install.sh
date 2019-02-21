284611220fb9d75fd9c0c8075de147179ef0d99c
Jim Zhang <jim.zhang@caicloud.io>
2019-02-21 11:23:44 +0800

    refactor(vim): adjust the installation logic of vim on centos

--- vim/install.sh
+++ vim/install.sh
@@ -2,6 +2,9 @@
 
 source $(dirname $(dirname ${BASH_SOURCE}))/framework/oo-bootstrap.sh
 
+namespace vim
+Log::AddOutput vim ERROR
+
 cd ${plugins}
 
 MINIMUM_VIM_VERSION="8.0"
@@ -16,50 +19,60 @@ if [[ $(OS::LSBDist) == "macos" ]]; then
     util::brew_install_one macvim --with-override-system-vim
     pyenv local --unset
 
+    # 可以选择安装
+    # brew tap neovim/neovim
+    # util::brew_install --HEAD neovim
+    # util::pip_install neovim
+
 elif [[ $(OS::LSBDist) == 'centos' ]]; then
+    VIM_VERSION=""
+
     if Command::Exists vim; then
-        vim_version="$(vim --version | head -1 | cut -f 5 -d " ")"
-        if [[ "${MINIMUM_VIM_VERSION}" != $(echo -e "${MINIMUM_VIM_VERSION}\n${vim_version}" | sort -s -t. -k 1,1n -k 2,2n | head -n1) ]]; then
+        VIM_VERSION="$(vim --version | head -1 | cut -f 5 -d " ")"
+    fi
 
-            # ruby  - ryenv
-            # python3 - pyenv
-            sudo yum install -y lua lua-devel luajit \
-                luajit-devel ctags git tcl-devel \
-                perl perl-devel perl-ExtUtils-ParseXS \
-                perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
-                perl-ExtUtils-Embed
-            # This step is needed to rectify an issue with how Fedora 20 installs XSubPP:
-            # symlink xsubpp (perl) from /usr/bin to the perl dir
-            sudo ln -s /usr/bin/xsubpp /usr/share/perl5/ExtUtils/xsubpp
-
-            git::clone https://github.com/vim/vim.git
-            cd vim
-
-            make distclean # if you build Vim before
-            ./configure --with-features=huge \
-                --enable-multibyte \
-                --enable-rubyinterp=yes \
-                --enable-python3interp=yes \
-                --with-python3-config-dir=$(python3-config --configdir) \
-                --enable-perlinterp=yes \
-                --enable-luainterp=yes \
-                --enable-gui=gtk2 \
-                --enable-cscope \
-                --prefix=/usr/local
-            sudo make install
-            cp src/vim /usr/local/bin/vim
+    if [[ ${VIM_VERSION} == "" ]] || (($(semver::compare ${VIM_VERSION} ${MINIMUM_VIM_VERSION}) == -1)); then
+        # ruby  - rbenv
+        if ! Command::Exists rbenv; then
+            Log "ruby is required in compiling vim, please install rbenv firstly"
+            exit 1
         fi
+        if ! Command::Exists pyenv; then
+            Log "python is required in compiling vim, please install pyenv firstly"
+            exit 1
+        fi
+        # python3 - pyenv
+        sudo yum install -y lua lua-devel luajit \
+            luajit-devel ctags git tcl-devel \
+            perl perl-devel perl-ExtUtils-ParseXS \
+            perl-ExtUtils-XSpp perl-ExtUtils-CBuilder \
+            perl-ExtUtils-Embed
+        # This step is needed to rectify an issue with how Fedora 20 installs XSubPP:
+        # symlink xsubpp (perl) from /usr/bin to the perl dir
+        sudo ln -sf /usr/bin/xsubpp /usr/share/perl5/ExtUtils/xsubpp
+
+        git::clone https://github.com/vim/vim.git vim
+        cd vim
+
+        make distclean # if you build Vim before
+        ./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp=yes \
+            --enable-python3interp=yes \
+            --with-python3-config-dir=$(python3-config --configdir) \
+            --enable-perlinterp=yes \
+            --enable-luainterp=yes \
+            --enable-gui=gtk2 \
+            --enable-cscope \
+            --prefix=/usr/local
+        sudo make install
+        cp src/vim /usr/local/bin/vim
     fi
 fi
-
-# 可以选择安装
-# brew tap neovim/neovim
-# brew install --HEAD neovim
-# pip install neovim
 
 # use spacevim
 curl -sLf https://spacevim.org/install.sh | bash
 
 mkdir -p ${HOME}/.SpaceVim.d/autoload
 ln -sf ${DOT_ROOT}/vim/init.toml ${HOME}/.SpaceVim.d/init.toml
-ln -sf ${DOT_ROOT}/vim/zoumo.vim ${HOME}/.SpaceVim.d/autoload/zoumo.vim
+ln -sf ${DOT_ROOT}/vim/customize.vim ${HOME}/.SpaceVim.d/autoload/customize.vim
