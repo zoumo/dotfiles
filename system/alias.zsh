c7985f77d701a483b26bc125fc0f5e3716fe5d33
Jim Zhang <jim.zhang@caicloud.io>
2019-02-21 14:42:25 +0800

    feat(alias): add some git alias

--- system/alias.zsh
+++ system/alias.zsh
@@ -32,3 +32,12 @@ alias tmux="TERM=screen-256color tmux -2"
 # --cache=$HOME/.npm/.cache/cnpm \
 # --disturl=https://npm.taobao.org/dist \
 # --userconfig=$HOME/.cnpmrc"
+
+# git alias
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.type 'cat-file -t'
+git config --global alias.dump 'cat-file -p'
