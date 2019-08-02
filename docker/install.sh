bfbca262338dad1a3e33d420c83ef87410cb1827
Jim Zhang <jim.zoumo@gmail.com>
2019-08-02 18:58:40 +0800

    feat(docker): install using script for linux

--- docker/install.sh
+++ docker/install.sh
@@ -12,16 +12,43 @@ fi
 
 if os::macos; then
     brew::cask::install docker
-elif [[ $(OS::LSBDist) == "centos" ]]; then
-    sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-    sudo yum install -y docker-ce
-    sudo systemctl start docker
+else
+    curl -fsSL https://get.docker.com | sudo bash
 
     # Manage Docker as a non-root user
     [[ -n $(cat /etc/group | cut -f 1 -d : | grep docker) ]] || sudo groupadd docker
     sudo usermod -aG docker $USER
 
+    sudo systemctl start docker
     # Configure Docker to start on boot
-    sudo systemctl enable docker
+    sudo systemctl enable docke
 fi
+
+# if [[ $(OS::LSBDist) == "centos" ]]; then
+#     sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+#     sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+#     sudo yum install -y docker-ce
+
+#     # Manage Docker as a non-root user
+#     [[ -n $(cat /etc/group | cut -f 1 -d : | grep docker) ]] || sudo groupadd docker
+#     sudo usermod -aG docker $USER
+
+#     sudo systemctl start docker
+#     # Configure Docker to start on boot
+#     sudo systemctl enable docker
+# elif [[ $(OS::LSBDist) == "debian" ]]; then
+#     # SET UP THE REPOSITORY
+#     sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
+#     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+#     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+#     # INSTALL DOCKER ENGINE - COMMUNITY
+#     sudo apt-get install -y docker-ce
+
+#     # Manage Docker as a non-root user
+#     [[ -n $(cat /etc/group | cut -f 1 -d : | grep docker) ]] || sudo groupadd docker
+#     sudo usermod -aG docker $USER
+
+#     sudo systemctl start docker
+#     # Configure Docker to start on boot
+#     sudo systemctl enable docke
+# fi
