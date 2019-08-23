7c541723e62bdbe4c65e8a7317706e22576177da
Jim Zhang <jim.zoumo@gmail.com>
2019-08-23 21:35:55 +0800

    feat: add protobuf

---
+++ protobuf/install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source $(dirname $(dirname ${BASH_SOURCE}))/framework/oo-bootstrap.sh
+
+namespace protobuf
+Log::AddOutput protobuf NOTE
+
+brew::install protobuf
+
+if Command::Exists go; then
+    # install protobuf
+    export GO111MODULE=on
+    # install golang protobuf lib
+    go get -v github.com/golang/protobuf/proto
+    # install golang protobuf generate plugin
+    go get -v github.com/golang/protobuf/protoc-gen-go
+
+    # gogo plugin
+    go get -v github.com/gogo/protobuf/protoc-gen-gogo
+    # gofast plugin
+    go get -v github.com/gogo/protobuf/protoc-gen-gofast
+
+    # gogo lib
+    go get -v github.com/gogo/protobuf/proto
+    go get -v github.com/gogo/protobuf/gogoproto
+
+    go get -v github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
+    go get -v github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
+fi
