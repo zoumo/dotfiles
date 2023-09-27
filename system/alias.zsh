e58a6cf9190f29d1b84a8487c2c37c148fd4f009
zoumo <jim.zoumo@gmail.com>
2023-09-27 18:09:36 +0800

    feat: alias kubectl to k

--- system/alias.zsh
+++ system/alias.zsh
@@ -13,16 +13,16 @@ fi
 #     set -g default-terminal "screen-256color"
 #     set -ag terminal-overrides ',screen-256color:Tc'
 #
-# TERM=xterm-256color tmux -2 启动的 session 中 
+# TERM=xterm-256color tmux -2 启动的 session 中
 # echo $TERM -> screen-256color 说明配置生效了
 # 但是 vim 的配色不正确, 这很奇怪
 #
-# TERM=screen-256color tmux -2 启动的 session 中 
+# TERM=screen-256color tmux -2 启动的 session 中
 # echo $TERM -> screen-256color
 # vim 的配色正确
-# 
+#
 # 只有启动 tmux 的时候 TERM 与 tmux 中相同，tmux 中的 vim 才能显示正确的配色
-# 
+#
 # tmux -2 force tmux to assume that the terminal support 256 colors
 alias tmuxinator="TERM=screen-256color tmuxinator"
 alias mux="tmuxinator"
@@ -41,3 +41,6 @@ git config --global alias.br branch
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
 git config --global alias.type 'cat-file -t'
 git config --global alias.dump 'cat-file -p'
+
+# kubectl alias
+alias k="kubectl"
