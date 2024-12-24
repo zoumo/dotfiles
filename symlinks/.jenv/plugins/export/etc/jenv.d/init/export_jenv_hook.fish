8f485314f073b797c1c313dc50c2beacef2625bb
zoumo <jim.zoumo@gmail.com>
2024-12-24 20:17:37 +0800

    feat: add jenv export plugin(tweaked)

---
+++ symlinks/.jenv/plugins/export/etc/jenv.d/init/export_jenv_hook.fish
@@ -0,0 +1,15 @@
+# load jenv and enable export hook
+function __jenv_export_hook --on-event fish_prompt
+  if not type -q jenv
+    return
+  end
+  set -gx JAVA_HOME (jenv javahome)
+  set -gx CLASS_PATH $JAVA_HOME/lib
+  set -gx JENV_FORCEJAVAHOME true
+
+  if test -e "$JAVA_HOME/bin/javac"
+    set -gx JDK_HOME "$JAVA_HOME"
+    set -gx CLASS_PATH $JAVA_HOME/lib
+    set -gx JENV_FORCEJDKHOME true
+  end
+end
